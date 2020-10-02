6e0c9d04868cf2665f925d455bed74667ab00844
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-01 21:54:25 -0300

    static load method for intuitive api on Router

--- core/Router.php
+++ core/Router.php
@@ -9,9 +9,16 @@ class Router
         $this->routes = $routes;
     }
 
+    public static function load($file)
+    {
+        $router = new static;
+        require $file;
+        return $router;
+    }
+
     public function direct($uri)
     {
-        if(array_key_exists($uri, $this->routes)){
+        if (array_key_exists($uri, $this->routes)) {
             return $this->routes[$uri];
         }
 
--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 
 $database = require_once "core/bootstrap.php";
 
-$router = new Router;
-
-require_once "routes.php";
-
 $URI = trim($_SERVER["REQUEST_URI"], "/");
+
+$router = Router::load("routes.php");
 
 require $router->direct($URI);
