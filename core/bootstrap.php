f4d141183a59327b696c0a78ca27edb69c0a38b9
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-01 22:00:13 -0300

    bootstraping Request class file

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,7 +4,7 @@ $config = require_once "config.php";
 
 require_once "core/database/Connection.php";
 require_once "core/database/QueryBuilder.php";
-
+require_once "core/Request.php";
 require_once "core/Router.php";
 
 return new QueryBuilder(
--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
 $database = require_once "core/bootstrap.php";
 
-require_once "core/Request.php";
-
-$URI = Request::uri();
-
 $router = Router::load("routes.php");
 
-require $router->direct($URI);
+require $router->direct(Request::uri());
